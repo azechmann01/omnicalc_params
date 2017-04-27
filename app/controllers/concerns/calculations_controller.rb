083e1e0a79cf159587fcb12ed0fb69c1f4947840
Andrea Zechmann <azechmann@chicagobooth.edu>
2017-04-26 19:24:32 -0500

    Half done with part 1

--- app/controllers/concerns/calculations_controller.rb
+++ app/controllers/concerns/calculations_controller.rb
@@ -1,17 +1,19 @@
 class CalculationsController < ApplicationController
-  def flex_square_5
+
+  def flex_square_num
     @user_number = params["num"].to_f
     @square = @user_number **2
     render("calculations/flex_square_5.html.erb")
   end
-end
 
-class CalculationsController <ApplicationController
-  def flex_square_root_8
+  def flex_square_root
     @user_number = params["num"].to_f
-    @square_root = @user_number / @user_number
-    render ("calculations/flex_square_root_8.html.erb")
-
+    @square_root = @user_number ** 0.5
+    render("calculations/flex_square_root.html.erb")
   end
 
+  def flex_present_value
+    # 
+  render("calculations/flex_present_value.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
+  get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_num"})
+  get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", {:controller => "calculations", :action => "flex_present_value"})
 end
